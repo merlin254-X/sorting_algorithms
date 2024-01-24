1a9357d9cbfd4c62ad2c3b32774fe9c6131163d9
merlin254-X <merlinderick8@gmail>
2024-01-24 02:16:57 -0800

    333

--- sort.h
+++ sort.h
@@ -39,4 +39,11 @@ void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
+
+/*task 2*/
+void quick_sort(int *array, size_t size);
+void quicksort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
+
+void shell_sort(int *array, size_t size);
 #endif
