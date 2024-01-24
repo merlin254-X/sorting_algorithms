90947707b7756bc9e4e76da81b40176bb7195db4
merlin254-X <merlinderick8@gmail>
2024-01-24 00:45:38 -0800

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,7 +2,7 @@
 
 /**
  * bubble_sort - Sorts an array of integers in ascending order
- *				 using the Bubble sort algorithm.
+ * using the Bubble sort algorithm.
  * @array: The array to be sorted.
  * @size: The size of the array.
  */
